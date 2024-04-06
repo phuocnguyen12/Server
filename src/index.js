8e475d9ed79af09b8d9820f84eb67b69d1077fe8
Nguyễn Thiên Phước <83675378+phuocnguyen12@users.noreply.github.com>
2024-04-06 21:51:56 +0700

    test

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const routes = require("./routes");
+const dotenv = require("dotenv");
+const dbConnect = require("./config/dbconnect");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3515;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["POST", "PUT", "GET", "DELETE"],
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+dbConnect();
+
+routes(app);
+
+app.listen(port, () => {
+  console.log("Server running on port: " + port);
+});
